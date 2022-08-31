1b871b914ffc50e7d01e7de6fe61e7fd09b9a3c7
Arvin Visi <arvinvisi03@gmail.com>
2022-08-31 19:39:20 +0430

    add related to book fk in Comment model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
 
 admin.site.register(Book)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'datetime_created', )
