ee109d12a40dafba5161190812b7cb7bcb59cd47
Arvin Visi <arvinvisi03@gmail.com>
2022-09-03 09:29:23 +0430

    add user to book model who created the book object

--- books/models.py
+++ books/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 
 class Book(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     description = models.TextField()
